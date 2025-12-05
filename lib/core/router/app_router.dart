e9b501f34ee5e71a63d4232272e866ec6f2345fb
JosDamQ <damian.garcia.12.2004@gmail.com>
2025-12-05 15:45:06 -0600

    feat(add): Structure for proyects and install first dependencies

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,8 @@
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/login',
+  routes: [
+    // Aquí se agregan las rutas de la aplicación
+  ],
+);
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData light = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+    scaffoldBackgroundColor: Colors.white,
+  );
+}
